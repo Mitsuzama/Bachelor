a31646b542a9cdc490af582c221c3ff366672d98
ElenaDenisa-Chelarasu <elena.chelarasu@yahoo.com>
2023-05-30 13:44:10 +0300

    json logger functional

--- Assets/Scripts/Logger/DataLoger.cs
+++ Assets/Scripts/Logger/DataLoger.cs
@@ -3,140 +3,90 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using Item;
-//using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace Logger
 {
     public class JSONData
     {
-        public EventData eventData;
+        public string timestamp;
+        public string tip_actiune;
+        public float durata;
         public int _id;
         public string nume;
         public string descriere;
         public float pret;
         public NutritionalInfo nutritional_info;
-        //public List<EventData> vector_actiuni;
     }
-    public class EventData
-    {
-        public string Timestamp { get; set; }
-        public string ActionType { get; set; }
-        public float ActionDuration { get; set; }
-    }
+
 
     public static class DataLogger
     {
+        public static JSONData CreateJsonDocument(int tip_actiune, float durata, ItemInfo itemInfo)
+        {
+            JSONData jsonDocument = new JSONData();
+
+            jsonDocument.timestamp = System.DateTime.UtcNow.ToString("yyyy-MM-dd");
+            jsonDocument.tip_actiune = (tip_actiune == 1) ? "TEMPTATION" : "BUY"; ;
+            jsonDocument.durata = durata;
+            jsonDocument._id = itemInfo.GetInstanceID();
+            jsonDocument.nume = itemInfo.ItemName;
+            jsonDocument.descriere = itemInfo.ItemDescription;
+            jsonDocument.pret = itemInfo.ItemPrice;
+            jsonDocument.nutritional_info = itemInfo.ItemNutritionalInfo;
+
+            return jsonDocument;
+        }
+
         public static void SaveEventsToJson(int actionType, float duration, ItemInfo itemInfo)
         {
             string saveFilePath = "SavedEvents.json";
-            //string saveFilePath = "Assets/Scripts/Logger/SavedEvents.json";
-            JSONData savedData;
-
-            if (File.Exists(saveFilePath))
-            {
-                savedData = JsonUtility.FromJson<JSONData>(File.ReadAllText(saveFilePath));
-                //savedData.vector_actiuni = new List<EventData>();
-
-                bool objectExists = (savedData._id == itemInfo.GetInstanceID() ) && (savedData.nume == itemInfo.ItemName);
-
-                //daca obiectul nu exista -> creez unul
-                if (!objectExists)
-                {
-                    
-                    savedData._id = itemInfo.GetInstanceID();
-                    savedData.nume = itemInfo.ItemName;
-                    savedData.descriere = itemInfo.ItemDescription;
-                    savedData.pret = itemInfo.ItemPrice;
-                    savedData.nutritional_info = itemInfo.ItemNutritionalInfo;
-                }
-
-                EventData newAction = new EventData();
-                newAction.ActionType = (actionType == 1) ? "TEMPTATION" : "BUY";
-                Debug.Log("ActionType" + newAction.ActionType.ToString());
-                newAction.Timestamp = System.DateTime.UtcNow.ToString("yyyy-MM-dd");
-                Debug.Log("timestamp" + newAction.Timestamp.ToString());
-                newAction.ActionDuration = duration;
-                Debug.Log("duration" + newAction.ActionDuration.ToString());
-
-                //savedData.vector_actiuni.Add(newAction);
-                savedData.eventData = newAction;
-                Debug.Log("newAction: " + newAction.ToString());
-                //Debug.Log("savedData.vector_actiuni: " + savedData.vector_actiuni.ToString());
-            }
-            else
-            {
-                JSONData newData = new JSONData();
-                //newData.vector_actiuni = new List<EventData>();
-
-                newData._id = itemInfo.GetInstanceID();
-                newData.nume = itemInfo.ItemName;
-                newData.descriere = itemInfo.ItemDescription;
-                newData.pret = itemInfo.ItemPrice;
-                newData.nutritional_info = itemInfo.ItemNutritionalInfo;
-
-                EventData newAction = new EventData();
-                newAction.ActionType = (actionType == 1) ? "TEMPTATION" : "BUY";
-                newAction.Timestamp = System.DateTime.UtcNow.ToString("yyyy-MM-dd");
-                newAction.ActionDuration = duration;
-
-                newData.eventData = newAction;
-
-                //newData.vector_actiuni.Add(newAction);
-
-                savedData = newData;
-            }
-
-            string json = JsonUtility.ToJson(savedData, true);
-            Debug.Log("ACESTA E JSONUL: " + json);
-            File.WriteAllText(saveFilePath, json);
-        }
-        
-
-        /*public static void SaveEventsToJson(int actionType, float duration, ItemInfo itemInfo)
-        {
-            Debug.Log("AM INTRAT");
-            string saveFilePath = "SavedEvents.json";
             List<JSONData> savedDataList;
+            Debug.Log("AM INTRAT");
 
-            if (File.Exists(saveFilePath))
+            if (itemInfo == null)
             {
-                string jsonData = File.ReadAllText(saveFilePath);
-                //savedDataList = JsonUtility.FromJson<List<JSONData>>(jsonData);
-                savedDataList = JsonConvert.DeserializeObject<List<JSONData>>(jsonData);
+                throw new ArgumentNullException("SaveEventsToJson: itemInfo");
             }
-            else
+
+            if (!File.Exists(saveFilePath))
+            {
+                throw new FileNotFoundException("The file was not found!", saveFilePath);
+            }
+
+            var length = new System.IO.FileInfo(saveFilePath).Length;
+            if (length == 0) //daca nu am nimic scris in fisier, initializez
             {
                 savedDataList = new List<JSONData>();
             }
-
-            JSONData savedData = savedDataList
-                .Find(data => data._id == itemInfo.GetInstanceID() && data.nume == itemInfo.ItemName);
-
-            if (savedData == null)
+            else // daca am ceva in fisier, preiau datele
             {
-                savedData = new JSONData();
-                savedData._id = itemInfo.GetInstanceID();
-                savedData.nume = itemInfo.ItemName;
-                savedData.descriere = itemInfo.ItemDescription;
-                savedData.pret = itemInfo.ItemPrice;
-                savedData.nutritional_info = itemInfo.ItemNutritionalInfo;
-
-                savedData.vector_actiuni = new List<EventData>();
-                savedDataList.Add(savedData);
+                string readText = File.ReadAllText(saveFilePath);
+                savedDataList = JsonConvert.DeserializeObject<List<JSONData>>(readText);
+                string sss = JsonConvert.SerializeObject(savedDataList, Formatting.Indented);
+                Debug.Log("ACESTA E savedDataList: " + sss);
             }
 
-            EventData newAction = new EventData();
-            newAction.ActionType = (actionType == 1) ? "TEMPTATION" : "BUY";
-            newAction.Timestamp = System.DateTime.UtcNow.ToString("yyyy-MM-dd");
-            newAction.ActionDuration = duration;
+            try
+            {
+                JSONData newEntry = CreateJsonDocument(actionType, duration, itemInfo);
+                string output = JsonConvert.SerializeObject(newEntry);
+                Debug.Log("ACESTA E output: " + output);
+                savedDataList.Add(newEntry);
 
-            savedData.vector_actiuni.Add(newAction);
+                string json = JsonConvert.SerializeObject(savedDataList, Formatting.Indented);
 
-            //string json = JsonUtility.ToJson(savedDataList, true);
-            string json = JsonConvert.SerializeObject(savedDataList, Formatting.Indented);
 
-            Debug.Log("ACESTA E JSONUL: " + json);
-            File.WriteAllText(saveFilePath, json);
-        }*/
+                Debug.Log("ACESTA E JSONUL: " + json);
+                File.WriteAllText(saveFilePath, json);
+
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex);
+            }
+            
+        }
     }
+
 }
